8b4392156ba9c2a00963fb7101adfd519ae1af71
Daniel Kesler <kesler.daniel@gmail.com>
2017-05-25 09:59:00 +0200

    Under developent messages

--- views/sensors_tab.php
+++ views/sensors_tab.php
@@ -1,5 +1,8 @@
 <div class="tab-pane fade in <?php echo $tab_active?'active':''; ?>" id="<?php echo $tab_id;?>-tab" data-attribute="<?php echo $tab_id;?>">
 	<div>
-		<?php echo $tab_content; ?>
+		<!-- <?php echo $tab_content; ?> -->
+		<div class="alert alert-warning animated  fadeIn" role="alert">
+			<i class="fa fa-warning"></i> This section is under development
+		</div>
 	</div>
 </div>
